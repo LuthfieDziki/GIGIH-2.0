aeb39a4116eb3c2562a09d37aa55b97c1883fe19
LuthfieDziki <luthfiedziki781@gmail.com>
2022-04-18 22:45:56 +0700

    HW-12

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <body>
+      <div className="card-login">
+        <Home />
+      </div>
+    </body>
+  );
+}
+
+export default App;
